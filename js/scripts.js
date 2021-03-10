2254600f9a155cd0e86bd784a35177628ec57935
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-03-11 00:31:01 +0300

    adde a step  form

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-filterSelection("all") // Execute the function and show all columns
+filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
   }
 }
 
-// Show filtered elements
-function w3AddClass(element, name) {
+
+function addClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -22,8 +22,8 @@ function w3AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
+
+function removeClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -34,8 +34,78 @@ function w3RemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
+/* this part  better work its  00:12 i  need sleep */
 
-// Add active class to the current button (highlight it)
+
+var currentTab = 0; // Current tab is the  first(0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+ 
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+ 
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  var x = document.getElementsByClassName("tab");
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab while in next
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+  //  the end of the form ,submit... :
+  if (currentTab >= x.length) {
+    
+    document.getElementById("contactForm").submit();
+    return false;
+  }
+  //  display the correct tab if not at th end of the tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  //  validation of the form 
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop for every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If field is empty then  add an invalid  class...
+    if (y[i].value == "") {
+      y[i].className += " invalid";
+      valid = false;
+    }
+  }
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; 
+}
+
+function fixStepIndicator(n) {//adds the "active" class to the current step and  remove "active from all steps":
+  
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  
+  x[n].className += " active";
+}
+
+
+/*
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -45,3 +115,12 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+function contactMe() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }*/
+
+  
