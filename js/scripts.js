5302c9f7f16090f585da813abd99c85877a3ec60
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-03-11 12:51:30 +0300

    add changes/Developerhtml

--- js/scripts.js
+++ js/scripts.js
@@ -34,17 +34,16 @@ function removeClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
-/* this part  better work its  00:12 i  need sleep */
 
 
-var currentTab = 0; // Current tab is the  first(0)
-showTab(currentTab); // Display the current tab
+var currentTab = 0; 
+showTab(currentTab); 
 
 function showTab(n) {
- 
+  
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
- 
+  
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
   } else {
@@ -55,72 +54,57 @@ function showTab(n) {
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
-  
+ 
   fixStepIndicator(n)
 }
 
 function nextPrev(n) {
+ 
   var x = document.getElementsByClassName("tab");
+ 
   if (n == 1 && !validateForm()) return false;
-  // Hide the current tab while in next
+ 
   x[currentTab].style.display = "none";
+ 
   currentTab = currentTab + n;
-  //  the end of the form ,submit... :
+ 
   if (currentTab >= x.length) {
-    
+ 
     document.getElementById("contactForm").submit();
     return false;
   }
-  //  display the correct tab if not at th end of the tab:
+  
   showTab(currentTab);
 }
 
 function validateForm() {
-  //  validation of the form 
+ 
   var x, y, i, valid = true;
   x = document.getElementsByClassName("tab");
   y = x[currentTab].getElementsByTagName("input");
-  // A loop for every input field in the current tab:
+  
   for (i = 0; i < y.length; i++) {
-    // If field is empty then  add an invalid  class...
+ 
     if (y[i].value == "") {
+      
       y[i].className += " invalid";
+    
       valid = false;
     }
   }
+  
   if (valid) {
     document.getElementsByClassName("step")[currentTab].className += " finish";
   }
   return valid; 
 }
 
-function fixStepIndicator(n) {//adds the "active" class to the current step and  remove "active from all steps":
+function fixStepIndicator(n) {
   
   var i, x = document.getElementsByClassName("step");
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
   }
-  
+ 
   x[n].className += " active";
 }
-
-
-/*
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-function contactMe() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }*/
-
-  
