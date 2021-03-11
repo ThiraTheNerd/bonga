f0b9afadc0183288995c4f950cc162019864405c
Natasha1594 <kinuthianatasha@gmail.com>
2021-03-11 09:49:19 +0300

    removed bonga child folder

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//Contact section
+$(document).ready(function() {
+    $("form#form34A").submit(function(event) {
+        // event.preventDefault();
+        var name = $("input#MERGE1").val();
+        var email = $("input#MERGE0").val();
+        var message = $("textarea#comment").val();
+        if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Please enter your name and email!");
+        }
+    });
+  });
+
+$(document).ready(function(){
+  $("#close-nav").click(function(){
+    $("#header").fadeOut();
+  });
+  $("#menu-icon").click(function(){
+    $("#header").fadeIn();
+  });
+});
